e3ed69fe8b69e7c2524e3e1a4aa2c14e40786a62
almuraduzzaman <almuraduzzaman.eee@gmail.com>
2023-05-26 09:50:30 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCtgIAzx0JIkzWX-beFqrefV8dNplrAxNQ",
+  authDomain: "dragon-news-f07f3.firebaseapp.com",
+  projectId: "dragon-news-f07f3",
+  storageBucket: "dragon-news-f07f3.appspot.com",
+  messagingSenderId: "526192602724",
+  appId: "1:526192602724:web:cc6686bac917d22eb6b971"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
---
+++ src/layouts/MainLayouts.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Header from '../Shared/Header/Header';
+import { Outlet } from 'react-router-dom';
+import Footer from '../Shared/Footer/Footer';
+import {Col,Container,Row} from 'react-bootstrap';
+import LeftNav from '../Shared/LeftNav/LeftNav';
+import RightNav from '../Shared/RightNav/RightNav';
+
+const MainLayouts = () => {
+    return (
+        <div>
+            <Header />
+            <Container>
+                <Row>
+                    <Col lg={3}>
+                        <LeftNav/>
+                    </Col>
+                    <Col lg={6}>
+                        <h2>Main Content comming</h2>
+                    </Col>
+                    <Col lg={3}>
+                        <RightNav/>
+                    </Col>
+                </Row>
+            </Container>
+            <Footer />
+        </div>
+    );
+};
+
+export default MainLayouts;
